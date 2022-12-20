f202982f7de876a9bda72d8570d0b2ff4d4f90a5
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-20 20:41:46 +0900

    add card

---
+++ client/components/Card/ViewCard.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  message: string;
+};
+
+export default function ViewCard({ message }: Props) {
+  return (
+    <div className="bg-slate-300 p-5 m-10">
+      <div>
+        <div className="bg-slate-400 mb-3">
+          <div>amount:</div>
+          <div>{message}</div>
+        </div>
+        <div className="bg-slate-400 mb-3">
+          <div>issuer:</div>
+          <div>{message}</div>
+        </div>
+        <div className="bg-slate-400 mb-3">
+          <div>recipient:</div>
+          <div>{message}</div>
+        </div>
+      </div>
+      <button className="bg-blue-400 float-right">button</button>
+    </div>
+  );
+}
--- client/pages/ViewBills.tsx
+++ client/pages/ViewBills.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import ViewCard from "../components/Card/ViewCard";
 import Layout from "../components/Layout/Layout";
 
 export default function ViewBills() {
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    let s: string[] = [];
+    for (let index = 0; index < 5; index++) {
+      s.push("akiyama");
+    }
+    setItems(s);
+  }, []);
+
   return (
     <body>
-      <Layout>ViewBills</Layout>
+      <Layout>
+        <div className="flex justify-around">
+          <div>
+            {items.map((message, index) => {
+              return (
+                <div key={index} className="flex justify-center">
+                  <ViewCard message={message} />
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {items.map((message, index) => {
+              return (
+                <div key={index} className="flex justify-center">
+                  <ViewCard message={message} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Layout>
     </body>
   );
 }
