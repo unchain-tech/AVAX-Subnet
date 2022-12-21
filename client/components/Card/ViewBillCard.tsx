f7627144be5a83a5deea7abffab363a2ebf68e70
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-21 19:00:47 +0900

    base viewBillcard

--- client/components/Card/ViewBillCard.tsx
+++ client/components/Card/ViewBillCard.tsx
@@ -1,20 +1,31 @@
 import SubmitButton from "../Button/SubmitButton";
 
+export type BillType = {
+  amount: string;
+  dueDate: string;
+  issuer: string;
+  recipient: string;
+  active: boolean;
+};
+
 type Props = {
   title: string;
   onClick: () => void;
+  bill: BillType;
 };
 
-export default function ViewBillCard({ title, onClick }: Props) {
+export default function ViewBillCard({ title, onClick, bill }: Props) {
   return (
     <div className="flex justify-center my-10">
       <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
         <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
-          Card title
+          Bill
         </h5>
+        <p className="text-gray-700 text-base mb-4">amount: {bill.amount}</p>
+        <p className="text-gray-700 text-base mb-4">dueDate: {bill.dueDate}</p>
+        <p className="text-gray-700 text-base mb-4">issuer: {bill.issuer}</p>
         <p className="text-gray-700 text-base mb-4">
-          Some quick example text to build on the card title and make up the
-          bulk of the card s content.
+          recipient: {bill.recipient}
         </p>
         <SubmitButton title={title} onClick={onClick} />
       </div>
--- client/pages/ViewBills.tsx
+++ client/pages/ViewBills.tsx
@@ -1,33 +1,45 @@
 import { useEffect, useState } from "react";
-import ViewBillCard from "../components/Card/ViewBillCard";
+import ViewBillCard, { BillType } from "../components/Card/ViewBillCard";
 import Layout from "../components/Layout/Layout";
 
 export default function ViewBills() {
-  const [billsOfRecipient, setBillsOfRecipient] = useState<string[]>([]);
-  const [billsOfIssuer, setBillsOfIssuer] = useState<string[]>([]);
+  const [billsOfRecipient, setBillsOfRecipient] = useState<BillType[]>([]);
+  const [billsOfIssuer, setBillsOfIssuer] = useState<BillType[]>([]);
 
-  const onClickCash = async (index: number, message: string) => {
+  const onClickCash = async (index: number, bill: BillType) => {
     alert(
-      "on click Cash:" + JSON.stringify(index) + ":" + JSON.stringify(message)
+      "on click Cash:" + JSON.stringify(index) + ":" + JSON.stringify(bill)
     );
   };
 
-  const onClickPay = async (index: number, message: string) => {
-    alert(
-      "on click Pay:" + JSON.stringify(index) + ":" + JSON.stringify(message)
-    );
+  const onClickPay = async (index: number, bill: BillType) => {
+    alert("on click Pay:" + JSON.stringify(index) + ":" + JSON.stringify(bill));
   };
 
   useEffect(() => {
-    let s: string[] = [];
+    let s: BillType[] = [];
     for (let index = 0; index < 5; index++) {
-      s.push("cashContent");
+      let c: BillType = {
+        amount: "10",
+        dueDate: "2022",
+        issuer: "issuer",
+        recipient: "recipient",
+        active: true,
+      };
+      s.push(c);
     }
     setBillsOfRecipient(s);
 
-    let p: string[] = [];
+    let p: BillType[] = [];
     for (let index = 0; index < 5; index++) {
-      p.push("payContent");
+      let c: BillType = {
+        amount: "10",
+        dueDate: "2022",
+        issuer: "issuer",
+        recipient: "recipient",
+        active: true,
+      };
+      p.push(c);
     }
     setBillsOfIssuer(p);
   }, []);
@@ -37,28 +49,30 @@ export default function ViewBills() {
       <Layout>
         <div className="flex justify-around bg-gradient-to-r from-sky-500 to-indigo-500">
           <div>
-            {billsOfRecipient.map((message, index) => {
+            {billsOfRecipient.map((bill, index) => {
               return (
                 <div key={index} className="flex justify-center">
                   <ViewBillCard
                     title="cash"
                     onClick={() => {
-                      onClickCash(index, message);
+                      onClickCash(index, bill);
                     }}
+                    bill={bill}
                   />
                 </div>
               );
             })}
           </div>
           <div>
-            {billsOfIssuer.map((message, index) => {
+            {billsOfIssuer.map((bill, index) => {
               return (
                 <div key={index} className="flex justify-center">
                   <ViewBillCard
                     title="pay"
                     onClick={() => {
-                      onClickPay(index, message);
+                      onClickPay(index, bill);
                     }}
+                    bill={bill}
                   />
                 </div>
               );
