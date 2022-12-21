5447a1c5ca917b640b1b31d1a0bbaad941120a30
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-21 19:52:36 +0900

    base admin

--- client/pages/Admin.tsx
+++ client/pages/Admin.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
+import SubmitButton from "../components/Button/SubmitButton";
 import ViewAccountCard from "../components/Card/ViewAccountCard";
-import AdminForm from "../components/Form/AdminForm";
+import InputField from "../components/Field/InputField";
 import Layout from "../components/Layout/Layout";
 
 export default function Admin() {
+  const [address, setAddress] = useState("");
   const [items, setItems] = useState<string[]>([]);
+
+  const onClickChange = async () => {
+    alert(JSON.stringify(address));
+  };
 
   useEffect(() => {
     let s: string[] = [];
@@ -17,17 +23,26 @@ export default function Admin() {
   return (
     <body>
       <Layout>
-        <div>
-          <AdminForm message={"out account"} />
-        </div>
-        <div>
-          {items.map((message, index) => {
-            return (
-              <div key={index} className="flex justify-center">
-                <ViewAccountCard message={message} />
-              </div>
-            );
-          })}
+        <div className="bg-gradient-to-r from-sky-500 to-indigo-500">
+          <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm h-fit my-10 mx-auto">
+            <form>
+              <InputField
+                label="Address to change access"
+                placeholder="address"
+                onChange={(value) => setAddress(value)}
+              />
+              <SubmitButton title="change" onClick={onClickChange} />
+            </form>
+          </div>
+          <div>
+            {items.map((message, index) => {
+              return (
+                <div key={index} className="flex justify-center">
+                  <ViewAccountCard message={message} />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </Layout>
     </body>
