b7754e9923977f7899bb161e347fe596a6994b5f
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-22 19:40:39 +0900

    admin

--- client/components/Form/AdminForm.tsx
+++ client/components/Form/AdminForm.tsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import { useCallback, useContext, useState } from "react";
+import CurrentAccountContext from "../../context/CurrentAccountProvider";
+import { useContract } from "../../hooks/useContract";
 import SubmitButton from "../Button/SubmitButton";
 import InputField from "../Field/InputField";
 
 export default function AdminForm() {
+  const [currentAccount] = useContext(CurrentAccountContext);
+  const { txAllowList } = useContract({ currentAccount });
   const [address, setAddress] = useState("");
 
-  const onClickChange = async () => {
-    alert(JSON.stringify(address));
-  };
+  const onEnable = useCallback(async () => {
+    if (!currentAccount) {
+      alert("connect wallet");
+      return;
+    }
+    if (!txAllowList) return;
+    try {
+      const txn = await txAllowList.setEnabled(address);
+      txn.wait();
+      alert("success");
+    } catch (error) {
+      // https://www.freecodecamp.org/news/object-object-in-javascript-meaning-in-js/
+      alert(JSON.stringify(error));
+    }
+  }, [currentAccount, txAllowList, address]);
+
+  const onNone = useCallback(async () => {
+    if (!currentAccount) {
+      alert("connect wallet");
+      return;
+    }
+    if (!txAllowList) return;
+    try {
+      const txn = await txAllowList.setNone(address);
+      txn.wait();
+      alert("success");
+    } catch (error) {
+      alert(JSON.stringify(error));
+    }
+  }, [currentAccount, txAllowList, address]);
 
   return (
     <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm h-fit mx-auto">
@@ -17,7 +48,10 @@ export default function AdminForm() {
           placeholder="address"
           onChange={(value) => setAddress(value)}
         />
-        <SubmitButton title="change" onClick={onClickChange} />
+        <div className="flex justify-between">
+          <SubmitButton title="enable" onClick={onEnable} />
+          <SubmitButton title="none" onClick={onNone} />
+        </div>
       </form>
     </div>
   );
--- client/pages/Admin.tsx
+++ client/pages/Admin.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import AllBillCards from "../components/Card/AllBillCards";
 import ViewAccountCard from "../components/Card/ViewAccountCard";
 import AdminForm from "../components/Form/AdminForm";
 import Layout from "../components/Layout/Layout";
@@ -18,21 +19,26 @@ export default function Admin() {
   return (
     <body>
       <Layout>
-        <div className="flex justify-center bg-gradient-to-r from-sky-500 to-indigo-500 h-screen">
-          <div className="my-20">
-            <AdminForm />
+        <div className="flex justify-around bg-gradient-to-r from-sky-500 to-indigo-500">
+          <div>
+            <AllBillCards />
           </div>
-          <div className="my-20 mx-20">
-            <div>dishonored</div>
-            {dishonoredAddresses.map((address, index) => {
-              return (
-                <div key={index} className="flex justify-center">
-                  <div className="my-2">
-                    <ViewAccountCard address={address} />
+          <div>
+            <div className="my-20">
+              <AdminForm />
+            </div>
+            <div className="my-20 mx-20">
+              <div>dishonored</div>
+              {dishonoredAddresses.map((address, index) => {
+                return (
+                  <div key={index} className="flex justify-center">
+                    <div className="my-1">
+                      <ViewAccountCard address={address} />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </Layout>
