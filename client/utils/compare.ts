f77d3c80613a827d7b8aabc0bb56e32aafcb2b31
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-24 09:12:09 +0900

    refactor

--- client/utils/compare.ts
+++ client/utils/compare.ts
@@ -1,3 +1,5 @@
 export const sameAddresses = (address1: string, address2: string) => {
+  // ブロックチェーンと接続する各APIの仕様によってアドレスを大文字で扱うものと小文字で扱うものがありますがその違いはアドレス値としては同じものです。
+  // その違いを吸収するために, アドレスをそれぞれ小文字にしてから比較します。
   return address1.toLocaleLowerCase() === address2.toLocaleLowerCase();
 };
