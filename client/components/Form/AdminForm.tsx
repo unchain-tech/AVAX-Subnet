96e4254dedcbc6d893bfc702ae51f9d979cfe4d9
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-24 17:21:01 +0900

    fix

--- client/components/Form/AdminForm.tsx
+++ client/components/Form/AdminForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from "react";
+import { useContext, useState } from "react";
 import CurrentAccountContext from "../../context/CurrentAccountProvider";
 import { useContract } from "../../hooks/useContract";
 import SubmitButton from "../Button/SubmitButton";
