4a2ccae40ee25597dd2fc09554236f2f02cc25b4
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-05-28 17:20:57 +0900

    refactor: use solhint

--- packages/contract/contracts/Bank.sol
+++ packages/contract/contracts/Bank.sol
@@ -29,20 +29,20 @@ contract Bank {
     address[] public dishonoredAddresses;
 
     // 各アドレスがコントラクトにロックしたトークンの数を保有します。
-    mapping(address => uint256) private balance;
+    mapping(address => uint256) private _balance;
 
     // 手形の期間を定数で用意します。
-    uint256 public constant term = 1 days * 60;
+    uint256 public constant TERM = 1 days * 60;
 
     // 割引金利
-    uint256 public constant discountRate = 10;
+    uint256 public constant DISCOUNTRATE = 10;
 
     // 手形手数料
-    uint256 public constant interestRate = 10;
+    uint256 public constant INTERESTRATE = 10;
 
     constructor() payable {}
 
-    function sendToken(address payable _to, uint256 _amount) private {
+    function _sendToken(address payable _to, uint256 _amount) private {
         (bool success, ) = (_to).call{value: _amount}("");
         require(success, "Failed to send token");
     }
@@ -56,13 +56,13 @@ contract Bank {
     }
 
     function getBalance() public view returns (uint256) {
-        return balance[msg.sender];
+        return _balance[msg.sender];
     }
 
     function beforeDueDate(uint256 _id) public view returns (bool) {
         Bill memory bill = allBills[_id];
 
-        if (block.timestamp <= bill.timestamp + term) {
+        if (block.timestamp <= bill.timestamp + TERM) {
             return true;
         } else {
             return false;
@@ -73,7 +73,7 @@ contract Bank {
         Bill memory bill = allBills[_id];
 
         if (beforeDueDate(_id)) {
-            return (bill.amount * (100 - discountRate)) / 100;
+            return (bill.amount * (100 - DISCOUNTRATE)) / 100;
         }
 
         return bill.amount;
@@ -81,7 +81,7 @@ contract Bank {
 
     function getAmountToPayBill(uint256 _id) public view returns (uint256) {
         Bill memory bill = allBills[_id];
-        return (bill.amount * (100 + discountRate)) / 100;
+        return (bill.amount * (100 + DISCOUNTRATE)) / 100;
     }
 
     function issueBill(uint256 _amount, address _recipient) public {
@@ -111,7 +111,7 @@ contract Bank {
 
         uint256 amount = getAmountToCashBill(_id);
 
-        sendToken(payable(msg.sender), amount);
+        _sendToken(payable(msg.sender), amount);
     }
 
     function lockToken(uint256 _id) public payable {
@@ -122,7 +122,7 @@ contract Bank {
 
         bill.status = BillStatus.Paid;
 
-        balance[msg.sender] += msg.value;
+        _balance[msg.sender] += msg.value;
     }
 
     function completeBill(uint256 _id) public payable {
@@ -139,8 +139,8 @@ contract Bank {
 
         uint256 amount = getAmountToPayBill(_id);
 
-        if (amount <= balance[bill.issuer]) {
-            balance[bill.issuer] -= amount;
+        if (amount <= _balance[bill.issuer]) {
+            _balance[bill.issuer] -= amount;
             bill.status = BillStatus.Completed;
         } else {
             bill.status = BillStatus.Dishonored;
